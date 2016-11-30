42fcb41daf4b402ff8ba32965fa48d2f3816fe34
Luis Fraile <lfraile@lfraile.net>
2016-11-30 17:03:18 +0100

    B.2

--- service-b/Startup.cs
+++ service-b/Startup.cs
@@ -41,7 +41,7 @@ namespace ServiceB
             app.ApplicationServices.GetService<TelemetryClient>().Context.Properties["Service name"] = "service-b";
             app.Run(async context =>
             {
-                await context.Response.WriteAsync("Hello from service B.1 running on " + Environment.MachineName);
+                await context.Response.WriteAsync("Hello from service B.2 running on " + Environment.MachineName);
             });
         }
     }
